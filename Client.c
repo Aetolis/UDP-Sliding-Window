d419cabc256464e74ea87f8699f586a39c95a771
haffent1 <50844313+haffent1@users.noreply.github.com>
2022-04-04 14:43:38 -0400

    Update Client.c

--- Client.c
+++ Client.c
@@ -1,2 +1,14 @@
 //Client.c
 #include "Shared.h"
+
+//read from file
+//set up own port/strucks
+//make connection
+
+//recive inital set up from server
+
+//send information
+
+//send ack
+
+//send packet
