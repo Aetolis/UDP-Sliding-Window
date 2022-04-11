9353f53bb6a0e1e1b97c66ce4ceb00c2d6756326
Aetolis <12118983+Aetolis@users.noreply.github.com>
2022-04-11 14:42:14 -0400

    Define additional constants

--- swp.h
+++ swp.h
@@ -1,6 +1,8 @@
-#define SERVER_PORT "4950"
+#define UDP_PORT "4950"
 #define MAXBUFLEN 100
 #define MAX_DATA 1024
+#define INIT_SEQ_NUM 0
+#define MAX_RETRY 5
 
 // global variables for signal handler
 int sockfd;
