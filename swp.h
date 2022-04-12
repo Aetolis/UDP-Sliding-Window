d7f1d28753d2ced829dd50e2c59cc6fe7b89db84
Aetolis <12118983+Aetolis@users.noreply.github.com>
2022-04-12 18:22:18 -0400

    Define new constants

--- swp.h
+++ swp.h
@@ -1,7 +1,8 @@
 #define UDP_PORT "4950"
-#define MAXBUFLEN 100
-#define MAX_DATA 1024
+#define MAX_DATA_LEN 1024
 #define INIT_SEQ_NUM 0
+#define MAX_SEQ_NUM 50
+#define WINDOW_SIZE 25
 #define MAX_RETRY 5
 
 // global variables for signal handler
