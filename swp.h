228a3aba45b53aaf50479981dd9497181828e445
Aetolis <12118983+Aetolis@users.noreply.github.com>
2022-04-15 13:30:41 -0400

    Update header

--- swp.h
+++ swp.h
@@ -1,5 +1,7 @@
 #define UDP_PORT "4950"
-#define MAX_DATA_LEN 1024
+#define HEADER_SIZE 8
+#define MAX_DATA_SIZE 1024
+#define MAX_PACKET_SIZE 1032
 #define INIT_SEQ_NUM 0
 #define MAX_SEQ_NUM 50 
 #define WINDOW_SIZE 25 //this can be half the size of seqnum, which can itself use 4 bytes 
