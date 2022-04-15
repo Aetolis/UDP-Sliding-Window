6d186f339d2a2b607e499b39c606f721a03c4430
Aetolis <12118983+Aetolis@users.noreply.github.com>
2022-04-15 13:01:11 -0400

    Update Sender

--- swpsender.cpp
+++ swpsender.cpp
@@ -4,26 +4,27 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <poll.h>
+#include <time.h>
 #include <string.h>
 #include <netdb.h>
 #include <arpa/inet.h>
 
 using namespace std;
 
-struct packet {
+typedef struct Packet {
     uint32_t seq_num;
-    bool ack_status;
+    // bool ack_status;
     uint16_t data_len;
-    // timer
-    char data[MAX_DATA_SIZE];
-};
+    char packet[MAX_PACKET_SIZE];
+    clock_t timestamp;
+} Packet;
 
 
 class SWPSender{ 
     public:
         int connect(char *hostname);
         int send_file(char *filename);
-        int disconnect();
+        int disconnect(uint32_t seq_num, char *filename);
 
     private:
         // UDP variables
@@ -31,15 +32,16 @@ class SWPSender{
         int status;
         int numbytes;
         struct addrinfo hints, *addr_ptr;
-        struct pollfd pfds[2]; //data structure describing a polling request
+        struct pollfd pfds[1]; //data structure describing a polling request
 
         // SWP variables
+        uint32_t packet_num;
         int LAR; //last ack recived
         int LFS; //last frame sent
         int windowSize; 
 
         // Buffer variables
-        char *send_buf[WINDOW_SIZE];
+        Packet send_buf[WINDOW_SIZE];
 
         // Helper functions
 
@@ -48,6 +50,19 @@ class SWPSender{
 };
 
 // Constructor
+SWPSender::SWPSender(){
+    packet_num = 0;
+    LAR = 0;
+    LFS = 0;
+    // windowSize = WINDOW_SIZE;
+
+    // Initialize the send buffer
+    for (int i = 0; i < WINDOW_SIZE; i++){
+        // send_buf[i].ack_status = false;
+        send_buf[i].data_len = 0;
+        send_buf[i].timestamp = 0;
+    }
+}
 
 // Connect
 int SWPSender::connect(char *hostname) {
@@ -80,10 +95,8 @@ int SWPSender::connect(char *hostname) {
     }
 
     // setup poll
-    pfds[0].fd = 0; //this is being changed to another fd
+    pfds[0].fd = sock_fd;
     pfds[0].events = POLLIN;
-    pfds[1].fd = sock_fd;
-    pfds[1].events = POLLIN;
 
     freeaddrinfo(client_info);
 
@@ -97,7 +110,7 @@ int SWPSender::connect(char *hostname) {
     // Set ACK flag as 0
     con_buf[4] = 0x00;
 
-    // Set Connection setup flag as 1
+    // Set control flag as 0x01
     con_buf[5] = 0x01;
 
     // Set length as 0
@@ -117,12 +130,12 @@ int SWPSender::connect(char *hostname) {
         }
 
         // Wait for response
-        if (poll(pfds, 2, 1000) == -1) {
+        if (poll(pfds, 1, 1000) == -1) {
             fprintf(stderr, "[Sender] Connection attempt #%d: failed to poll\n", i);
             continue;
         }
 
-        if (pfds[1].revents & POLLIN) {
+        if (pfds[0].revents & POLLIN) {
             if (recvfrom(sock_fd, recv_buf, 8, 0, NULL, 0) == -1) {
                 fprintf(stderr, "[Sender] Connection attempt #%d: failed to recieve initial connection response\n", i);
                 continue;
@@ -191,25 +204,184 @@ int SWPSender::send_file(char *filename) {
         exit(1);
     }
 
-    // Get file size
-    fseek(fp, 0, SEEK_END);
-    int file_size = ftell(fp);
-    fseek(fp, 0, SEEK_SET);
+    bool EOF_flag = false;
+    int cur_len;
+    char *read_buf[MAX_DATA_SIZE];
+    uint32_t temp_seq_num;
+    uint16_t temp_data_len;
 
-    //initilize buffer / turn file into an array of structs
-    //while loop until file ends
+    while (!EOF_flag) {
+        // Check for ACKs
+        if (poll(pfds, 1, -1) == -1) {
+            fprintf(stderr, "[Sender] failed to poll\n");
+            exit(1);
+        }
+
+        if (pfds[0].revents & POLLIN) {
+            // Receive ACK
+            char recv_buf[8];
+            if (recvfrom(sock_fd, recv_buf, 8, 0, NULL, 0) == -1) {
+                fprintf(stderr, "[Sender] failed to receive ACK\n");
+            }
+
+            // Check if ACK flag is set
+            else if (recv_buf[4] != 0x01) {
+                fprintf(stderr, "[Sender] ACK flag not set\n");
+            }
+
+            // Check if connection setup flag is valid
+            else if (recv_buf[5] != 0x01) {
+                fprintf(stderr, "[Sender] connection setup flag not valid\n");
+            }
+
+            // Check if length is valid
+            //if ((recv_buf[6] > 0x01 && recv_buf[7]  0x00)|| recv_buf[6] != 0x00 || recv_buf[7] != 0x00) {
+            else if (recv_buf[6] != 0x00 || recv_buf[7] != 0x00) {
+                fprintf(stderr, "[Sender] invalid length\n");
+            }
+
+            else {
+                // Check if sequence number is valid
+                uint32_t recv_seq_num;
+                memcpy(&recv_seq_num, recv_buf, sizeof(uint32_t));
+                recv_seq_num = ntohl(recv_seq_num);
+                if (recv_seq_num > MAX_SEQ_NUM || recv_seq_num <= LAR || recv_seq_num > LFS) {
+                    fprintf(stderr, "[Sender] invalid sequence number\n");
+                } else {
+                    // ACK received
+                    fprintf(stdout, "[Sender] ACK received for sequence number %d\n", recv_seq_num);
+                    for (int i = LAR + 1; i <= recv_seq_num; i++) {
+                        send_buf[i].data_len = 0;
+                        memset(send_buf[i].packet, 0, MAX_PACKET_SIZE);
+                    }
+                    LAR = recv_seq_num;
+                    LFS = (recv_seq_num + WINDOW_SIZE) % MAX_SEQ_NUM;
+                }
+            }
+        }
+
+        // Populate buffer
+        for (int i = LAR + 1; i <= LAR + WINDOW_SIZE; i++) {
+            if (send_buf[i].data_len == 0) {
+                // Set packet sequence number
+                send_buf[i].seq_num = packet_num % MAX_SEQ_NUM;
+                packet_num++;   
+                
+                // Read file into buffer
+                cur_len = fread(read_buf, 1, MAX_DATA_SIZE, fp);
+                if (cur_len < MAX_DATA_SIZE || cur_len == 0) {
+                    EOF_flag = true;
+                }
+                send_buf[i].data_len = (uint32_t)cur_len;
+
+                // Set packet sequence number
+                temp_seq_num = htonl(send_buf[i].seq_num);
+                memcpy(send_buf[i].packet, &temp_seq_num, sizeof(uint32_t));
+
+                // Set ACK flag to 0x00
+                send_buf[i].packet[4] = 0x00;
+
+                // Set control flag to 0x00
+                send_buf[i].packet[5] = 0x00;
+
+                // Set length of data
+                temp_data_len = htons(send_buf[i].data_len);
+                memcpy(send_buf[i].packet + 6, &temp_data_len, sizeof(uint16_t));
+
+                // Copy data into packet buffer
+                memcpy(send_buf[i].packet + 8, read_buf, cur_len);
+            } 
+            
+            if (sendto(sockfd, send_buf[i].packet, HEADER_SIZE + send_buf[i].data_len, 0, addr_ptr->ai_addr, addr_ptr->ai_addrlen) == -1) {
+                fprintf(stderr, "[Sender] failed to send packet #%d\n", send_buf[i].seq_num);
+            }
+            fprintf(stdout, "[Sender] sent packet #%d\n", send_buf[i].seq_num);
+
+            if (EOF_flag) {
+                break;
+            }
+        }
+    }
 
 
-
-    //begin sending, but also we need to check ttl as we are sending 
-
-    //do we need to tell reciver that we are done sending
 
 
 }//end_send file
 
 
-int SWPSender::disconnect(char *filename) {
+int SWPSender::disconnect(uint32_t final_seq_num, char *filename) {
+    // Establish virtual connection
+    char disc_buf[8]; //8 bytes bc sequnum is 4, ack is 1, control is 1, and length is 2
+
+    // Set initial sequence number
+    uint32_t seq_num = htonl((uint32_t)final_seq_num);
+    memcpy(disc_buf, &seq_num, sizeof(uint32_t)); 
+
+    // Set ACK flag as 0
+    disc_buf[4] = 0x00;
+
+    // Set control flag as 0x02
+    disc_buf[5] = 0x02;
+
+    // Set length as 0
+    disc_buf[6] = 0x00;
+    disc_buf[8] = 0x00;
+
+    // Send connection request
+    char recv_buf[8];
+    bool acked = false;
+    fprintf(stdout, "[Sender] Establishing connection with receiver...\n");
+
+    // Retry connection a set number of times
+    for (int i = 0; i < MAX_RETRY; i++) { 
+        if (sendto(sockfd, disc_buf, 8, 0, addr_ptr->ai_addr, addr_ptr->ai_addrlen) == -1) {
+            fprintf(stderr, "[Sender] Connection attempt #%d: failed to send initial connection request\n", i);
+            continue;
+        }
+
+        // Wait for response
+        if (poll(pfds, 1, 1000) == -1) {
+            fprintf(stderr, "[Sender] Connection attempt #%d: failed to poll\n", i);
+            continue;
+        }
+
+        if (pfds[0].revents & POLLIN) {
+            if (recvfrom(sock_fd, recv_buf, 8, 0, NULL, 0) == -1) {
+                fprintf(stderr, "[Sender] Connection attempt #%d: failed to recieve initial connection response\n", i);
+                continue;
+            }
+
+            // Check if initial sequence number is correct
+            uint32_t recv_seq_num;
+            memcpy(&recv_seq_num, recv_buf, sizeof(uint32_t));
+            if (ntohl(recv_seq_num) != final_seq_num) {
+                fprintf(stderr, "[Sender] Connection attempt #%d: invalid initial sequence number\n", i);
+                continue;
+            }
+
+            // Check if ACK flag is set
+            if (recv_buf[4] != 0x01) {
+                fprintf(stderr, "[Sender] Connection attempt #%d: ACK flag not set\n", i);
+                continue;
+            }
+
+            // Check if control flag is valid
+            if (recv_buf[5] != 0x02) {
+                fprintf(stderr, "[Sender] Connection attempt #%d: connection setup flag not valid\n", i);
+                continue;
+            }
+
+            // Check if length is valid
+            //if ((recv_buf[6] > 0x01 && recv_buf[7]  0x00)|| recv_buf[6] != 0x00 || recv_buf[7] != 0x00) {
+            if (recv_buf[6] != 0x00 || recv_buf[7] != 0x00) {
+                fprintf(stderr, "[Sender] Connection attempt #%d: invalid length\n", i);
+                continue;
+            }
+
+            // Connection request successful
+            acked = true;
+            break;
+        }
 
     //same code as connect, but we are just sending diffrent info
 
