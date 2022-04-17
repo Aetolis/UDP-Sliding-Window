0db45438bfe33f47f239375f7ea279603c922fbd
Aetolis <12118983+Aetolis@users.noreply.github.com>
2022-04-17 14:20:42 -0400

    Update sender tests

--- Sender.cpp
+++
@@ -1,1 +0,0 @@
-#include <swp.h>
---
+++ sender.cpp
@@ -0,0 +1,9 @@
+// sender.cpp
+// Test file for SWPSender class
+
+#include "swp.h"
+
+int main (void) {
+    SWPSender sender;
+    sender.connect("localhost");
+}
